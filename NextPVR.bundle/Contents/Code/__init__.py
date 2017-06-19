451896fdbfdeccedcc57494c6fbb4616f99f262d
psyciknz <github@andc.co.nz>
2017-06-20 10:25:41 +1200

    GUI updates and Channel Groups
    Can now set a default view if you don't want the normal 3 options.
    Added a Channel List - with a default specified in prefs.  Also an
    option for listing all channel groups and viewing channels for each

--- NextPVR.bundle/Contents/Code/__init__.py
+++ NextPVR.bundle/Contents/Code/__init__.py
@@ -39,6 +39,9 @@ def Start():
 	#PVR_URL = 'http://%s:%s/' % (Prefs['server'],Prefs['port'])
 	Log('URL set to %s' % PVR_URL)
     
+	Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	
 	try:
 		# Get curret server version and save it to dict.
 		server_version = XML.ElementFromURL(PMS_URL % '', errors='ignore').attrib['version']
@@ -58,39 +61,62 @@ def Start():
 @handler('/video/nextpvr','NextPVR')
 def MainMenu():
 	
-    Log('Client %s' % Request.Headers)
-    clientident = ''
-    try:
-        clientident = Request.Headers['X-Plex-Client-Identifier']
-    except:
-        Log('Could not get client details')
- 
-    Log('Client Details: ident:%s' %  clientident)
-    dir=ObjectContainer()
-    Log('MainMenu: Adding Live Menu')
-    dir.add(DirectoryObject(key=Callback(LiveMenu), title='Live',thumb=R('live.jpg')))
-    Log('MainMenu: Adding Channel Menu')
-    dir.add(DirectoryObject(key=Callback(ChannelMenu), title='Channel',thumb=R('live.jpg')))
+	Log('Client %s' % Request.Headers)
+	clientident = ''
+	try:
+		clientident = Request.Headers['X-Plex-Client-Identifier']
+	except:
+		Log('Could not get client details')
+	
+	mainmenuformat = "All"
+	try:
+		mainmenuformat = Prefs["mainmenu"]
+	except:
+		Log('Could not get menui format preferences')
+		pass
+		
+	Log('Client Details: ident:%s' %  clientident)
+	
+	if mainmenuformat == "Live":
+		dir=ObjectContainer()
+		dir=LiveMenu()
+	elif mainmenuformat == "Channel Group (set below)":
+		dir=ObjectContainer()
+		dir=ChannelListMenu()
+	elif mainmenuformat == "Channel Group List":
+		dir=ObjectContainer()
+		dir=ChannelGroupMenu()
+	else:
+		dir=ObjectContainer()
+		Log('MainMenu: Adding Live Menu')
+		dir.add(DirectoryObject(key=Callback(LiveMenu), title='Live',thumb=R('live.jpg')))
+		#Log('MainMenu: Adding Channel Menu')
+		#dir.add(DirectoryObject(key=Callback(ChannelMenu), title='Channel',thumb=R('live.jpg')))
+		Log('MainMenu: Adding Channel List Menu')
+		dir.add(DirectoryObject(key=Callback(ChannelListMenu), title='Channel List',thumb=R('live.jpg')))
+		dir.add(DirectoryObject(key=Callback(ChannelGroupMenu), title='Channel Group List',thumb=R('live.jpg')))
 
-    #http://192.168.1.100:8866/streamer/vlc/stream.aspx?url=/live?channel=3
-    #dir.add(
-	#	CreateVideoClipObject(
-	#		url = PVR_URL + 'services/service?method=channel.transcode.initiate&channel_id=7224&profile=720p-1024kbps',
-	#		title = 'TestLive3',
-	#		summary='summary',
-	#		rating_key=float(3),
-	#		channel=3
-	#		)
-	#	)
-    Log('MainMenu: Live Menu Added')
-    dir.add(PrefsObject(title="Preferences", summary="Configure how to connect to NextPVR", thumb=R("icon-prefs.png")))
-    Log('MainMenu: URL set to %s' % PVR_URL)
-    return dir
+		#http://192.168.1.100:8866/streamer/vlc/stream.aspx?url=/live?channel=3
+		#dir.add(
+		#	CreateVideoClipObject(
+		#		url = PVR_URL + 'services/service?method=channel.transcode.initiate&channel_id=7224&profile=720p-1024kbps',
+		#		title = 'TestLive3',
+		#		summary='summary',
+		#		rating_key=float(3),
+		#		channel=3
+		#		)
+		#	)
+		dir.add(PrefsObject(title="Preferences", summary="Configure how to connect to NextPVR", thumb=R("icon-prefs.png")))
+		Log('MainMenu: Live Menu Added')
+		
+	
+	Log('MainMenu: URL set to %s' % PVR_URL)
+	return dir
 
 ####################################################################################################	
 @route('/video/nextpvr/live')
 def LiveMenu():
-	oc = ObjectContainer(title2='Live')
+	oc = ObjectContainer(title2='Live', view_group='Details')
 
 	clientident = 'temp'
 	try:
@@ -111,6 +137,7 @@ def LiveMenu():
 	
 	# Nodes with start_time > stime which is x number of days ago
 	channels = root.findall('listings/channel')
+	Log('LiveMenu: Number of channel entries found: %d' % len(channels))
 	shows = []
 	for channel in channels:
 		Log('LiveMenu: **********************************************************************************************************')
@@ -124,21 +151,35 @@ def LiveMenu():
 		programme = channel.find('l')
 		summary = channelname
 		if programme is None:
-			programmname = channelname
+			title = channelname
 		else:
 			Log('LiveMenu: Looking for programme name for channel %s' % channelname)
-			programmname = channelname + ' : ' + programme.find('name').text
-			summary = programme.find('description').text
+			titleformat = Prefs["titleformat"]
+			Log('LiveMenu: Looking for title format')
+			if titleformat == "Programme Name":
+				title = programme.find('name').text
+			elif titleformat == "Channel + Programme Name":
+				title = channelname + ' : ' + programme.find('name').text
+			elif titleformat == "Channel + Channel Callsign + Programme Name":
+				title = channelname + ' (' + str(channelnumber) + ') : ' + programme.find('name').text
+			elif titleformat == "Call Sign + Programme Name":
+				title = str(channelnumber) + ' : ' + programme.find('name').text
+			else: 
+				title = channelname + ' : ' + programme.find('name').text
+			
+			Log('LiveMenu: Looking for programme name for channel %s' % title)
+			summarytext = programme.find('description').text
+			Log('LiveMenu: Looking for summary name for channel %s' % summarytext)
 
-		Log('LiveMenu: Channel name %s, Summary %s' % (programmname,summary))
+		Log('LiveMenu: Channel name %s, Summary %s' % (title,summarytext))
 		
 		testURL = PVR_URL + 'live?channel=%s&sid=plex&client=%s' % (channelnumber,clientident)
 		Log('LiveMenu: URL set to %s' % testURL)
 		oc.add(
 		CreateVideoClipObject(
 			url = testURL,
-			title = programmname,
-			summary=summary,
+			title = title,
+			summary=summarytext,
 			call_sign = channelname,
 			rating_key=float(channelnumber),
 			channel=channelid
@@ -195,6 +236,113 @@ def ChannelMenu():
       )
     return oc
 
+####################################################################################################	
+@route('/video/nextpvr/channellist')
+def ChannelListMenu(group=None):
+
+	clientident = 'temp'
+	oc = ObjectContainer()
+	try:
+		clientident = Request.Headers['X-Plex-Client-Identifier']
+	except:
+		Log('Could not get client details')
+	
+	Log('ChannelListMenu: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++: ')
+	
+	if not group is None:
+		Log('ChannelListMenu: Getting channel list for passed in group: ' + group)
+		chngroup = '&group_id=' + urllib2.quote(group)
+		oc = ObjectContainer(title2=group)
+	else:
+		Log('ChannelListMenu: Getting channel list from name in preferences')
+		channelgroup = Prefs['channelgroup']
+		if (not channelgroup is None):
+			chngroup = '&group_id=' + urllib2.quote(Prefs['channelgroup'])
+		else:
+			chngroup = ''
+		group = 'Empty'
+		oc = ObjectContainer(title2='Channel List')
+	
+	url = PVR_URL + 'services?method=channel.list%s&sid=plex&client=%s' % (chngroup,clientident)
+	
+	Log('ChannelListMenu: Loading URL %s' % url)
+	request = urllib2.Request(url, headers={"Accept" : "application/xml"})
+	Log('ChannelListMenu: Request: %s' % request)
+	u = urllib2.urlopen(request)
+	Log('ChannelListMenu: Result = %s code= %s' % ( u.code,u.msg))
+	tree = ET.parse(u)
+	#Log('ChannelListMenu: Loading listings from xml file')
+	#tree = ET.parse('g:\\plex\\listing.xml')
+	root = tree.getroot()
+	
+	# Nodes with start_time > stime which is x number of days ago
+	channels = root.findall('channels/channel')
+	Log('ChannelListMenu: Number of channel entries found: %d' % len(channels))
+	if (len(channels) > 0):
+		for channel in channels:
+			Log('ChannelListMenu: **********************************************************************************************************')
+			channelname = channel.find('name').text
+			channelnumber = float(channel.find('formatted-number').text)
+			channelid = channel.find('id').text
+			
+			Log('ChannelListMenu: Channel number \'%.2f\' name is \'%s\'' % (channelnumber, channelname))
+			
+			Log('ChannelListMenu: Getting first programme for %s' % channelname)
+			summary = channelname
+			programmname = channelname
+			
+			Log('ChannelListMenu: Channel name %s, Summary %s' % (programmname,summary))
+			
+			testURL = PVR_URL + 'live?channel=%s&sid=plex&client=%s' % (channelnumber,clientident)
+			Log('ChannelListMenu: URL set to %s' % testURL)
+			oc.add(
+			CreateVideoClipObject(
+				url = testURL,
+				title = programmname,
+				summary=summary,
+				call_sign = channelname,
+				rating_key=float(channelnumber),
+				channel=channelid
+				)
+			)
+	
+	Log('ChannelListMenu: Completed listing for ' + group)
+	return oc
+
+####################################################################################################	
+@route('/video/nextpvr/channelgroups')
+def ChannelGroupMenu():
+	oc = ObjectContainer()
+
+	clientident = ''
+	try:
+	  clientident = Request.Headers['X-Plex-Client-Identifier']
+	except:
+	  Log('Could not get client details')
+
+	url = PVR_URL + 'public/GuideService/ChannelGroups'
+	Log('LiveMenu: Loading URL %s' % url)
+	parsed_json = json.load(urllib2.urlopen(url))
+
+	# Nodes with start_time > stime which is x number of days ago
+	listings = parsed_json['channelGroupJSONObject']['ChannelGroups']
+	for listing in listings:
+		Log('ChannelMenu: **********************************************************************************************************')
+		if not listing is None:
+			Log('ChannelGroupMenu: ' + listing)
+		
+			oc.add(
+			DirectoryObject(
+				key=Callback(ChannelListMenu,group=listing), 
+				title=listing,
+				thumb=R('live.jpg')
+				)
+			)
+			Log('ChannelGroupMenu: Completed ' + listing)
+		else:
+			Log('ChannelGroupMenu: Skipping entry')
+	
+	return oc
 ####################################################################################################
 @route('/video/nextpvr/videoclipobject')
 #def CreateVideoClipObject(url, title, summary, rating_key, channel=None, container='mp2ts', include_container=False, includeRelated=False,**kwargs):
@@ -203,9 +351,8 @@ def CreateVideoClipObject(url, title, summary, rating_key,call_sign='',  channel
 	showthumb = PVR_URL + 'service?method=recording.artwork&recording_id=%s&sid=plex' % str(rating_key)
 	Log('CreateVideoClipObject Getting artwork url "%s"' % showthumb)
 	if not channel is None:
-		Log('Getting logo the old way')
 		thumb = PVR_URL + 'services?method=channel.icon&channel_id=%s' % channel
-		Log('Getting logo the old way: ' + PVR_URL + 'services?method=channel.icon&channel_id=%s' % channel)
+		Log('Getting logo : ' + PVR_URL + 'services?method=channel.icon&channel_id=%s' % channel)
 		
 		if thumb is None:
 			thumb = R(call_sign + '.jpg')
@@ -217,6 +364,7 @@ def CreateVideoClipObject(url, title, summary, rating_key,call_sign='',  channel
 		thumb = R(ART)
 
 	Log('CreateVideoClipObject: Playvideo: %s' % url)
+	Log('CreateVideoClipObject: Playvideo: %s:%s' % (title,summary))
 	track_object = EpisodeObject(
 		key = Callback(CreateVideoClipObject, url=url, title=title, summary=summary, rating_key=rating_key,call_sign=call_sign,channel=channel,container=container,include_container=True, includeRelated=False,includeRelatedCount=False,includeBandwidths=True),
 		title = title ,
